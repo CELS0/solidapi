d78c3993546fa7a7b043bed8a4b24258f998d8b0
CELS0 <celsojr.300@gmail.com>
2021-09-14 21:09:04 -0300

    add jest

---
+++ src/__tests__/unit/CreateUserUserCase.spec.ts
@@ -0,0 +1,6 @@
+describe("Create user", () => {
+    it("Should be possible to create a user",()=>{
+
+        expect(2+2).toBe(4)
+    })
+})
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Router } from 'express';
+import express from 'express';
 import { router } from './routes';
 
 const app = express();
